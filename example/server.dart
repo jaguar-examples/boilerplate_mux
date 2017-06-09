26773f6db2cb7cf455c1f3b5a045ed5d93ad4be6
Ravi Teja Gudapati <tejainece@gmail.com>
2017-06-09 14:28:31 +0200

    Jaguar 0.5.x

--- example/server.dart
+++ example/server.dart
@@ -2,23 +2,20 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:jaguar/jaguar.dart';
-import 'package:jaguar/interceptors.dart';
 import 'package:jaguar_mux/jaguar_mux.dart';
 
 main() async {
   final muxer = new MuxBuilder();
 
-  muxer.get('/api/version', () => 0.1);
+  muxer.get('/api/version', (Context ctx) => 0.1);
 
-  muxer
-      .get(
-          '/api/info',
-          () => {
-                'name': 'jaguar',
-                'motto': 'Speed. Simplicity. Extensiblity.',
-                'degree of awesomeness': 'infinity!',
-              })
-      .wrap(new WrapEncodeMapToJson());
+  muxer.get(
+      '/api/info',
+      (Context ctx) => Response.json({
+            'name': 'jaguar',
+            'motto': 'Speed. Simplicity. Extensiblity.',
+            'degree of awesomeness': 'infinity!',
+          }));
 
   Mux mux = muxer.build();
 
